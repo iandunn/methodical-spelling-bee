463a98263b34d6bc4eef8050e80cab0a988587e3
Ian Dunn <ian@iandunn.name>
2022-12-03 18:51:26 -0800

    Partially fix observer dependency problem

--- src/contentScript/main.js
+++ src/contentScript/main.js
@@ -9,6 +9,8 @@ export function MethodicalSpellingBee() {
 	const [ ready, setReady ]     = useState( false );
 	const [ activeLetterList, setActiveLetterList ] = useState( {} );
 
+	const [ foo, setFoo ] = useState( 1 ); // tmp
+
 	// Fetch today's hints when component mounts.
 	useEffect( () => {
 		const todaysHints = getTodaysHints();
@@ -28,71 +30,38 @@ export function MethodicalSpellingBee() {
 				console.log( error );
 			}
 		);
-
-		// move this whole thing to a function for modularity? so this component would just have
-		// useEffect( foo, [] ) ?
-		// no b/c it's meant to be inside the caller so it has access to local vars/state. docs say passing state get/setters isn't great
 	}, [] );
-
-	// have to declare functions used by the effect inside the effect?
-	// that's kinda gross
-	// or maybe don't, but need to use useCallback do declare the functions, and then put them in the list?
-	// er no, they're defined outside the component, so they're already memoized or whatever?
-	// only need usecallback when the function would get recreated every time the effect is called?
-
-	// but maybe the closures inside the comoponet need to use usecallback? but they wouldn't be in the deps array b/c they're not named
-	// but that's fine b/c it'd only get called once if leaving the array empty
-	// doesn't matter if the effect only gets called once though? i dunno
-	//todo before next compile add usecallback to the closures above
-
-	//if can't the usecallback or moving everything inside the effcect dont fix it, then maybe post minimial example to stackoverflow
-		// ask how to get it working with your desired structure (funcs defined outside component, empty array argument to useeffect, etc)
-
 
 	// Register a DOM observer when component mounts.
 	// It will catch when new words are added to the found list, and update the scores.
 	// Observing the DOM instead of local storage, because the latter isn't meant to be done on the same page.
 	useEffect( () => {
-		//console.log( 'start of obrserver effect', activeLetterList );
-
-		// to const
-		//console.log( 'only register observer callback once' );
-			// so maybe i do need the dependency list?
-			// oh maybe not b/c this is just registering the callback, which should only happen once?
-			// can't remove the dep array like eslint wants, that makes this run 3x a second
-			// probably something i'm not understanding or structuring wrong
-
 		const targetNode = document.querySelector( '.sb-wordlist-items-pag' );
 
-		//before next compile, change this to use usecallback. probably wont help make acviteletterlist avavilablle, but still good anyway to prevent creating a ton of these functions and only 1 gets cleaned up or something
 		const callback = function ( mutationsList ) {
 			for ( const mutation of mutationsList ) {
 				if ( mutation.type === 'childList' ) {
-					console.log( activeLetterList, mutation.addedNodes[0].innerText ); // problem, activeletlist is emtpy here
-					// why?
-
-					const lettersList = bumpTwoLettersCount( activeLetterList, [ mutation.addedNodes[0].innerText ] ); // looks fine inside here
-					console.log( lettersList ); // maybe this is the problem?
-					// or maybe i need to manually trigger a rerender?
-					// or its a smell and i've got things architected wrong b/c if it were right then react would automatically rerender
-					// do i need to clone object ?
+					const lettersList = bumpTwoLettersCount( activeLetterList, [ mutation.addedNodes[0].innerText ] );
 
 					setActiveLetterList( lettersList );
+					setFoo( Date.now() ); // todo UI only updates b/c this is changing state every timethis makes the ui update, but without this it doesn't update? why?
+					// maybe b/c activeletterlist is part of dependency array? no, same happens even when in array
+					// so it's something else
+					// todo try changing callback function decleration to use usecallback()
 				}
 			}
 		};
 
+		// todo make sure this isn't registered multiple times
 		const observer = new MutationObserver( callback );
-
 		observer.observe( targetNode, { childList: true } );
 
 		return function cleanup() {
-			console.log( 'cleanup observer' );
 			observer.disconnect();
 		}
-	}, [] );
-	// need to include stuff in deps array ^ for this to work? test once things are cleand up
-	// prob need do use usecallback to makes sure this isn't registered a ton and memory leaks or whatever
+	}, [ activeLetterList ] );
+	// i think activeletterlist should be in ^, but not totally sure
+
 
 	if ( ! ready ) {
 		return (
